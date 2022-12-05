d148adda210d0d07ae9f305a60ef763185101c4a
bobthebuilder49 <117940970+bobthebuilder49@users.noreply.github.com>
2022-12-05 13:44:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
+})
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.forever(function () {
+	
+})
